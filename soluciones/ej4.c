e200933f49cfb4f2d8822e9b16c50a7ff495d404
danielnoblenava <danielnoble290@gmail.com>
2026-02-15 21:23:33 -0600

    Implement array address printing in ejercicio 4
    Added array declarations and a loop to print addresses.

--- soluciones/ej4.c
+++ soluciones/ej4.c
@@ -1,6 +1,21 @@
 
 #include <stdio.h>
+
+/*
+  Tamaño del tipo: 4 bytes
+  direccion de a[5] = 0x7ffc752c7d50
+  formula = Base + i x tam_tipo
+*/
 int main(){
-    // Resolver ejercicio 4
-    return 0;
+	int a[3];
+	int b[3];
+	int c[3];
+	
+	for (int i = 0; i < 3; i++){
+		int ra = 10 + i * sizeof(int);	
+		printf("\n&a[%d] = %d", i, ra);
+	}
+	printf("\na[5] = %p", &a[5]);
+	printf("\n");
+        return 0;
 }
