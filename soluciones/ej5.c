a8899a60660145ae749f0d8cff81c2cd484b136b
danielnoblenava <danielnoble290@gmail.com>
2026-02-15 21:24:00 -0600

    Add comments about memory access in ej5.c
    Added comments regarding memory access issue in exercise 5.

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -1,5 +1,11 @@
 
+
 #include <stdio.h>
+
+/*
+  No es correcto porque estas accediendo a una parte de memoria que
+  no le corresponde al array.
+*/
 int main(){
     // Resolver ejercicio 5
     return 0;
