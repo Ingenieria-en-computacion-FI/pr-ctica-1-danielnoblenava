bacb2e07c54ad22fb7e54d572f46a6b5640d755e
danielnoblenava <danielnoble290@gmail.com>
2026-02-15 21:22:34 -0600

    Implement memory address printing for arrays
    Added code to print memory addresses of arrays a, b, and c.

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,25 @@
 
 #include <stdio.h>
+
+/*
+  Las direcciones estan juntas en memoria y dependen de: 
+  1. Son arrays
+  2. Son almacenados en el stack
+*/
 int main(){
-    // Resolver ejercicio 3
-    return 0;
+    
+	int a[3], b[3], c[3];
+	printf("\nDirecciones de a: ");
+	for(int i = 0; i < 3; i++)
+		printf("\na[%d]: %p", i,  &a[i]);
+	
+	printf("\nDirecciones de b: ");
+	for(int i = 0; i < 3; i++)
+		printf("\nb[%d]: %p", i, &b[i]);
+	
+	printf("\nDirecciones de c: ");
+	for (int i = 0; i < 3; i++)
+		printf("\nc[%d]: %p", i, &c[i]);
+	printf("\n");
+        return 0;
 }
