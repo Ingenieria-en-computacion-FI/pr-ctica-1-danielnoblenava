03f2fca36279f61a788396dce03772e0e8bbf792
danielnoblenava <danielnoble290@gmail.com>
2026-02-18 23:53:12 -0600

    Update ej4.c

--- soluciones/ej4.c
+++ soluciones/ej4.c
@@ -3,19 +3,13 @@
 
 /*
   Tamaño del tipo: 4 bytes
-  direccion de a[5] = 0x7ffc752c7d50
+  direccion de a[5] = 0x7ffc752c7d50 o 5020
   formula = Base + i x tam_tipo
 */
 int main(){
 	int a[3];
-	int b[3];
-	int c[3];
 	
-	for (int i = 0; i < 3; i++){
-		int ra = 10 + i * sizeof(int);	
-		printf("\n&a[%d] = %d", i, ra);
-	}
-	printf("\na[5] = %p", &a[5]);
-	printf("\n");
-        return 0;
+	printf("\n&a[0] = %d", 5000+0*sizeof(a[0]));
+	printf("\n&a[3] = %d", 5000+3*sizeof(a[3]));	
+	return 0;
 }
